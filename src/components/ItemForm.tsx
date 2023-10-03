fdb9c02b784c455426951afcace0801e9c1657b5
Izarothy <izarothmail@gmail.com>
2023-10-03 17:55:38 +0200

    feat: enforce options on category, style form

--- src/components/ItemForm.tsx
+++ src/components/ItemForm.tsx
@@ -67,7 +67,12 @@ const ItemForm = () => {
         </label>
         <label>
           Category
-          <input type="text" {...register("category")} required />
+          <select {...register("category")} required>
+            <option value="Test 1">Test 1</option>
+            <option value="Test 2">Test 2</option>
+            <option value="Test 3">Test 3</option>
+            <option value="Test 4">Test 4</option>
+          </select>
         </label>
         <button
           type="submit"

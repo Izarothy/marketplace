1bbd4f8de2ed3922f918f889f125e6a6b5d4be45
Izarothy <izarothmail@gmail.com>
2023-10-04 13:22:24 +0200

    feat: add auth user image

--- next.config.mjs
+++ next.config.mjs
@@ -18,7 +18,7 @@ const config = {
     defaultLocale: "en",
   },
   images: {
-    domains: ["picsum.photos"],
+    domains: ["picsum.photos", "lh3.googleusercontent.com"],
   },
 };
 
--- src/components/AuthButton.tsx
+++ src/components/AuthButton.tsx
@@ -1,13 +1,23 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import React from "react";
+import Image from "next/image";
 
 const AuthButton = () => {
   const { data: sessionData } = useSession();
   const userName = sessionData?.user?.name?.split(" ")[0];
   return (
     <div className="flex items-center gap-2">
-      <p className="text-center text-lg text-white">
-        {sessionData && <span>{userName}</span>}
+      <p className="flex items-center gap-2 text-center text-lg text-white">
+        {sessionData && (
+          <Image
+            src={sessionData.user.image ?? "htps://picsum.photos/32/32"}
+            alt="User"
+            width={32}
+            height={32}
+            className="rounded-full"
+          />
+        )}
+        {sessionData && <span className="text-sm">{userName}</span>}
       </p>
       <button
         className="rounded-full bg-white/10 px-10 py-2 text-sm font-semibold text-white no-underline transition hover:bg-white/20"
